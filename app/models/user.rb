802719621ed70ecc8ffdd5957b196a5cfd9f866b
Faheem <faheem.ismail@qbitechs.com>
2022-02-21 17:32:14 +0500

    Add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
- 
+ validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i, message: "Email invalid"  },  uniqueness: { case_sensitive: false },length: { minimum: 4, maximum: 254 }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
